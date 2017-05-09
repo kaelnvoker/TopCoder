3dffa7bf62d0497e8660ccf7a4a3ab8c9d4bd2c7
kael <kaelqwexort@gmail.com>
2017-05-09 03:52:36 -0400

    First commit

---
+++ src/main/kael/AvoidRoads.java
@@ -0,0 +1,13 @@
+package kael;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AvoidRoads {
+
+    public static void main(String []args){
+        System.out.println("To a new start");
+        logger.info("you are doomed");
+    }
+    private static final Logger logger = LoggerFactory.getLogger(AvoidRoads.class);
+}
